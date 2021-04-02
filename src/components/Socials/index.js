6e6dd193e956b119cbd5a190d22434bd139348d5
Mohamed Abdelrahman <mohamedabdelrahman972@gmail.com>
2021-04-02 15:49:36 +0200

    update margin

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -76,7 +76,7 @@ const Socials = (props) => {
       </a>
 
        <a 
-       href='https://www.linkedin.com/in/aboelkassem' className='link hover-blue dib black-70' target='_blank' style="margin-left:12px;"
+       href='https://www.linkedin.com/in/aboelkassem' className='linkedin-prof link hover-blue dib black-70' target='_blank'
         title='aboelkassem on Linked In'
         rel='noopener nofollow noreferrer'
        >
