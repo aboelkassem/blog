5382a33dfaf37376f6b543c7d08d8d5e0346557e
Restyled.io <commits@restyled.io>
2023-11-04 18:25:40 +0000

    Restyled by prettier

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,39 +1,51 @@
-import {graphql, Link} from 'gatsby'
-import {kebabCase} from 'lodash'
-import PropTypes from 'prop-types'
-import React from 'react'
-import {Helmet} from 'react-helmet'
+import { graphql, Link } from "gatsby";
+import { kebabCase } from "lodash";
+import PropTypes from "prop-types";
+import React from "react";
+import { Helmet } from "react-helmet";
 
-import Layout from '../../components/Layout'
+import Layout from "../../components/Layout";
 
 const TagsPage = (props) => {
   const {
-    data : {allMarkdownRemark : {group}, site : {siteMetadata : {title}}}
-  } = props
+    data: {
+      allMarkdownRemark: { group },
+      site: {
+        siteMetadata: { title },
+      },
+    },
+  } = props;
 
-  return <Layout>
-         <Helmet title = {`Tags | ${
-              title}`} />
+  return (
+    <Layout>
+      <Helmet title={`Tags | ${title}`} />
 
-    <section className='mw7 center avenir'>
-      <header className='tc'>
-        <h1 className='f1 code mb1 fw1 dib tracked-tight'>Tags</h1><
-         /header>
+      <section className="mw7 center avenir">
+        <header className="tc">
+          <h1 className="f1 code mb1 fw1 dib tracked-tight">Tags</h1>
+        </header>
 
-      <ul className='list ph3 ph5-ns pv4 tc'>
-        {group.map(tag => (
-          tag.fieldValue && tag.fieldValue !== '/' && ( <
-             li key = {tag.fieldValue} className = 'dib mr1 mb2'>< Link
-  className = 'f6 f5-ns fw4 b db pa2 link dim dark-gray ba b--black-20'
-  to = {`/tags/${kebabCase(tag.fieldValue)}/`} > { tag.fieldValue } ({tag.totalCount})
-              </Link>
-            </li>
-          )
-        ))
-}</ul>
-
-    </section>< /Layout>
-}
+        <ul className="list ph3 ph5-ns pv4 tc">
+          {group.map(
+            (tag) =>
+              tag.fieldValue &&
+              tag.fieldValue !== "/" && (
+                <li key={tag.fieldValue} className="dib mr1 mb2">
+                  <Link
+                    className="f6 f5-ns fw4 b db pa2 link dim dark-gray ba b--black-20"
+                    to={`/tags/${kebabCase(tag.fieldValue)}/`}
+                  >
+                    {" "}
+                    {tag.fieldValue} ({tag.totalCount})
+                  </Link>
+                </li>
+              ),
+          )}
+        </ul>
+      </section>
+    </Layout>
+  );
+};
 
 TagsPage.propTypes = {
   data: PropTypes.shape({
@@ -46,22 +58,22 @@ TagsPage.propTypes = {
       group: PropTypes.array,
     }),
   }),
-}
+};
 
-export default TagsPage
+export default TagsPage;
 
 export const tagPageQuery = graphql`
-    query TagsQuery {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-        allMarkdownRemark(limit: 1000) {
-            group(field: frontmatter___tags) {
-                fieldValue
-                totalCount
-            }
-        }
+  query TagsQuery {
+    site {
+      siteMetadata {
+        title
+      }
     }
-`
+    allMarkdownRemark(limit: 1000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,108 +1,121 @@
-import {graphql, Link} from 'gatsby'
-import PropTypes from 'prop-types'
-import React from 'react'
-import {Helmet} from 'react-helmet'
+import { graphql, Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+import { Helmet } from "react-helmet";
 
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 
-const TagRoute =
-    (props) => {
-      const {
-        data : {
-          site : {siteMetadata : {title}},
-          allMarkdownRemark : {totalCount, edges : posts}
-        },
-        pageContext : {tag}
-      } = props
+const TagRoute = (props) => {
+  const {
+    data: {
+      site: {
+        siteMetadata: { title },
+      },
+      allMarkdownRemark: { totalCount, edges: posts },
+    },
+    pageContext: { tag },
+  } = props;
 
-  const postLinks = posts.map(post => (
-    <div key={post.node.fields.slug} className='pv4 bb bt b--black-10 ph3 ph0-l'>
-      <Link to={post.node.fields.slug} className='link dim no-underline black'>
+  const postLinks = posts.map((post) => (
+    <div
+      key={post.node.fields.slug}
+      className="pv4 bb bt b--black-10 ph3 ph0-l"
+    >
+      <Link to={post.node.fields.slug} className="link dim no-underline black">
         <div>
           <img
             src={post.node.frontmatter.cover.publicURL}
             alt={post.node.frontmatter.title}
-            className='w-60'
+            className="w-60"
           />
         </div>
-        <h2 className='f4 f3-m f2-l f-subheadline-l measure lh-title fw1 mt0 mb0'>{post.node.frontmatter.title}</h2>
+        <h2 className="f4 f3-m f2-l f-subheadline-l measure lh-title fw1 mt0 mb0">
+          {post.node.frontmatter.title}
+        </h2>
       </Link>
     </div>
-  ))
+  ));
 
-            const tagHeader = `${totalCount} post${
-                totalCount === 1 ? '' : 's'} tagged with “${tag}”`
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? "" : "s"
+  } tagged with “${tag}”`;
 
-            return (
-                <Layout><section className = 'pa3'>
-                <Helmet title =
-                 {
-                   `${tag} | ${title}`
-                 } />
+  return (
+    <Layout>
+      <section className="pa3">
+        <Helmet title={`${tag} | ${title}`} />
 
-        <section className='mw7 center avenir'>
-          <div className='tc'>
-            <h3 className='f2 lh-title fw4 mb3 mt0 pt3 bw2 avenir'>{tagHeader}</h3>
+        <section className="mw7 center avenir">
+          <div className="tc">
+            <h3 className="f2 lh-title fw4 mb3 mt0 pt3 bw2 avenir">
+              {tagHeader}
+            </h3>
 
-                <div className = 'taglist'>{postLinks} <
-                /div>
+            <div className="taglist">{postLinks} </div>
 
-            <p className='mb5'>
-              <Link to='/tags /
-                        ' className=' no -
-                    underline black dim avenir'> Browse all
-                        tags</Link>
-            </p></div>
+            <p className="mb5">
+              <Link
+                to="/tags /
+                        "
+                className=" no -
+                    underline black dim avenir"
+              >
+                {" "}
+                Browse all tags
+              </Link>
+            </p>
+          </div>
         </section>
-                    </section>
-    </Layout>)
-    }
+      </section>
+    </Layout>
+  );
+};
 
-               TagRoute.propTypes = {
-      pageContext : PropTypes.shape({
-        tag : PropTypes.string,
+TagRoute.propTypes = {
+  pageContext: PropTypes.shape({
+    tag: PropTypes.string,
+  }),
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string,
       }),
-      data : PropTypes.shape({
-        site : PropTypes.shape({
-          siteMetadata : PropTypes.shape({
-            title : PropTypes.string,
-          }),
-        }),
-        allMarkdownRemark : PropTypes.shape({
-          edges : PropTypes.array,
-          totalCount : PropTypes.number,
-        }),
-      }),
-    }
+    }),
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+      totalCount: PropTypes.number,
+    }),
+  }),
+};
 
-export default TagRoute
+export default TagRoute;
 
 export const tagPageQuery = graphql`
-    query TagPage($tag: String) {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-        allMarkdownRemark(
-            limit: 1000
-            sort: { fields: [frontmatter___date], order: DESC }
-            filter: { frontmatter: { tags: { in: [$tag] } } }
-        ) {
-            totalCount
-            edges {
-                node {
-                    fields {
-                        slug
-                    }
-                    frontmatter {
-                        title,
-                        cover {
-                          publicURL
-                        }
-                    }
-                }
-            }
-        }
+  query TagPage($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
     }
-`
+    allMarkdownRemark(
+      limit: 1000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            cover {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`;
